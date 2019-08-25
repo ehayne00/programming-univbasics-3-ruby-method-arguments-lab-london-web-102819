e540915fb29b5c9f478ced1ee5c06db7dbf36951
Emily Haynes <ehayne00@hotmail.com>
2019-08-25 16:22:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,9 @@ def #introduction(name)
  
 def #introduction_with_language(name, language)
  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+ 
+def introduction_with_language_optional(name, language = "ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 
 
 
